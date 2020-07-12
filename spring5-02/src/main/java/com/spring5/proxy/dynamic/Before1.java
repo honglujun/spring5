f020ea7da2a5c055beb6065b5f1b57adfde30a49
honglujun <909408954@qq.com>
2020-07-12 16:49:21 +0800

    p78--动态代理的细节分析

--- spring5-02/src/main/java/com/spring5/proxy/dynamic/Before1.java
+++ spring5-02/src/main/java/com/spring5/proxy/dynamic/Before1.java
@@ -8,13 +8,18 @@ public class Before1 implements MethodBeforeAdvice {
     /**
      * 作用：需要把运行在原始方法执行之前运行的额外功能，书写在before方法中
      *
-     * @param method
-     * @param objects
-     * @param o
+     * @param method 额外功能所增加给的那个原始方法
+     *               login方法
+     *               register方法
+     *               showOrder方法
+     * @param objects 额外功能所增加的那个原始方法的参数。String name，String password
+     *                                              User user
+     * @param target 额外功能所增加的那个原始对象 UserServiceImpl
+     *                                       OrderServiceImpl
      * @throws Throwable
      */
     @Override
-    public void before(Method method, Object[] objects, Object o) throws Throwable {
+    public void before(Method method, Object[] objects, Object target) throws Throwable {
         System.out.println("==========new method before advice log===================");
     }
 }
